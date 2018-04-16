0736879243eb8f3ef995dcb8c028a0e39d84dc4a
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-16 12:56:39 +0200

    add doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2018 The K8s-Ingress Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+K8s manages Ingresses based on a list.
+*/
+package k8s_ingress
