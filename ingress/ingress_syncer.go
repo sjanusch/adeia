a382562a8e7530f63471efaaa666cf7f8be890a0
Kevin Wiesmüller <kwiesmueller@seibert-media.net>
2018-04-16 16:02:18 +0200

    add basic project structure and ci/cd

---
+++ ingress/ingress_syncer.go
@@ -0,0 +1,23 @@
+// Copyright 2018 The K8s-Ingress Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ingress
+
+import "github.com/seibert-media/k8s-ingress/model"
+
+//go:generate counterfeiter -o ../mocks/ingress_fetcher.go --fake-name IngressFetcher . fetcher
+type fetcher interface {
+	Fetch() ([]model.Domain, error)
+}
+
+// Syncer creates ingress for a list of domains
+type Syncer struct {
+	Fetcher fetcher
+}
+
+// Sync fetches a list of domains an create ingresses
+func (i *Syncer) Sync() error {
+	var _, err = i.Fetcher.Fetch()
+	return err
+}
