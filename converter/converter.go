9eacfc86ed2533f783a0e17991d2ffe0ca957f82
mlorenz <lorenz.swt@gmail.com>
2018-04-17 10:20:44 +0200

    k8s-ingress: refactored to use multiple rules in ingress instead of multiple ingresses

--- converter/converter.go
+++ converter/converter.go
@@ -4,33 +4,56 @@ import (
 	"github.com/seibert-media/k8s-ingress/model"
 	"k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 type Converter struct {
 	Domains []model.Domain
-	Ingress []v1beta1.Ingress
+	Ingress v1beta1.Ingress
+
 }
 
-func (c *Converter) Convert() ([]v1beta1.Ingress, error) {
+var (
+	serverNamePtr = "test"
+	serverPortPtr = "8080"
+	namePtr = "GT"
+	namespacePtr = "GT-NAMESPACE"
+)
+
+func (c *Converter) Convert() (v1beta1.Ingress, error) {
+	var ingressRules []v1beta1.IngressRule
 	for _, domain := range c.Domains {
-		ingress := v1beta1.Ingress{
-			ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{
-					"kubernetes.io/ingress.class": "traefik",
-				},
-			},
-			Spec: v1beta1.IngressSpec{
-				Rules: []v1beta1.IngressRule{
-					{
-						Host: string(domain),
-						IngressRuleValue: v1beta1.IngressRuleValue{
+
+		ingressRule := v1beta1.IngressRule{
+			Host: string(domain),
+			IngressRuleValue: v1beta1.IngressRuleValue{
+				HTTP: &v1beta1.HTTPIngressRuleValue{
+					Paths: []v1beta1.HTTPIngressPath{
+						{
+							Path: "/",
+							Backend: v1beta1.IngressBackend{
+								ServiceName: serverNamePtr,
+								ServicePort: intstr.Parse(serverPortPtr),
+							},
 						},
 					},
 				},
 			},
 		}
-		c.Ingress = append(c.Ingress, ingress)
+		ingressRules = append(ingressRules, ingressRule)
 	}
 
-	return c.Ingress, nil
+	var ingress = v1beta1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"kubernetes.io/ingress.class": "traefik",
+			},
+			Name:      namePtr,
+			Namespace: namespacePtr,
+		},
+		Spec: v1beta1.IngressSpec{
+			Rules: ingressRules,
+		},
+	}
+	return ingress, nil
 }
