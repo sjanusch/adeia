62b300ae4c7bc79f93df8f2559e53acadeab72a3
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-18 13:32:10 +0200

    create or update ingress

--- ingress/k8s_applier.go
+++ ingress/k8s_applier.go
@@ -9,6 +9,7 @@ import (
 	k8s_v1beta1 "k8s.io/api/extensions/v1beta1"
 	k8s_kubernetes "k8s.io/client-go/kubernetes"
 	k8s_clientcmd "k8s.io/client-go/tools/clientcmd"
+	k8s_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // K8sApplier add ingress to Client.
@@ -27,6 +28,11 @@ func (a *K8sApplier) Apply(ingress *k8s_v1beta1.Ingress) error {
 	if err != nil {
 		return errors.Wrap(err, "create k8s config failed")
 	}
-	_, err = clientset.ExtensionsV1beta1().Ingresses(a.Namespace).Create(ingress)
+	_, err = clientset.ExtensionsV1beta1().Ingresses(a.Namespace).Get(ingress.Name, k8s_v1.GetOptions{})
+	if err != nil {
+		_, err = clientset.ExtensionsV1beta1().Ingresses(a.Namespace).Create(ingress)
+		return err
+	}
+	_, err = clientset.ExtensionsV1beta1().Ingresses(a.Namespace).Update(ingress)
 	return err
 }
